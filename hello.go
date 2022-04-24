502ddf29aa40a6a1d12415eadfe21c62043fcc8e
小草 <980754689@qq.com>
2022-04-24 15:52:51 +0800

    go变量定义—1

--- hello.go
+++ hello.go
@@ -4,8 +4,8 @@ import "fmt"
 func initValue(){
     var a int
     var s string
-    // fmt.Printf("%d %q/n", a, s)
-    fmt.Println(a, s)
+    fmt.Printf("%d %q\n", a, s)
+    // fmt.Println(a, s)
 }
 
 func setValue(){
@@ -14,10 +14,20 @@ func setValue(){
     fmt.Println(a, b, s)
 }
 
+func setValueOne(){
+    a, b, c, s := 1, 2, false, "abc"
+    b = 3
+    c = true
+    a = 22
+    s = 33
+    fmt.Println(a, b, c, s)  
+}
+
 func main() {
     fmt.Println("Hello, World!")
     fmt.Println("测试代码")
 
     initValue()
     setValue()
+    setValueOne()
 }
