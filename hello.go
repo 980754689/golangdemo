64ea00be80fa140f1cfdda060081f53faf2e522f
小草 <980754689@qq.com>
2022-04-24 15:42:50 +0800

    初始化代码

--- hello.go
+++ hello.go
@@ -1,8 +1,23 @@
 package main
 
 import "fmt"
+func initValue(){
+    var a int
+    var s string
+    // fmt.Printf("%d %q/n", a, s)
+    fmt.Println(a, s)
+}
+
+func setValue(){
+    var a, b int = 2, 3
+    var s string = "abc"
+    fmt.Println(a, b, s)
+}
 
 func main() {
     fmt.Println("Hello, World!")
     fmt.Println("测试代码")
+
+    initValue()
+    setValue()
 }
