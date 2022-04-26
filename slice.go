44bf595db31946d7a36d171139c04250faac66df
小草 <980754689@qq.com>
2022-04-26 12:10:25 +0800

    切片扩展-cap

--- slice.go
+++ slice.go
@@ -29,12 +29,22 @@ func main() {
 	updateSlice(arr[:])
 	fmt.Println("updateSlice(arr[:]) = ", arr[:])
 
-    arr2 := [...]int{1,2,3,4,5,6,7,8,9,10}
+	arr2 := [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	s2 := arr2[2:6]
 	s3 := s2[3:5]
 	s4 := s2[3:7]
 	s5 := s2[1:5]
 
-	fmt.Println(s2,s3,s4,s5)
+	fmt.Println(s2, s3, s4, s5)
+
+	arr2[0], arr2[1] = 100, 200
+	fmt.Println(arr2)
+	s6 := arr2[1:6]
+	s7 := s6[3:7]
+	s8 := s6[:7]
+	fmt.Println(s6, s7, s8)
+	fmt.Printf("s6 = %v len(s6) = %d cap(s6) = %d\n", s6, len(s6), cap(s6))
+	fmt.Printf("s7 = %v len(s7) = %d cap(s7) = %d\n", s7, len(s7), cap(s7))
+	fmt.Printf("s8 = %v len(s8) = %d cap(s8) = %d\n", s8, len(s8), cap(s8))
 
 }
